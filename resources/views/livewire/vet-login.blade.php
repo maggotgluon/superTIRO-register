9ec083c165be8800229de82edf9c471932e0566a
Ruttaphong <maggotgluon@gmail.com>
2023-10-15 13:28:49 +0700

    login screen

--- resources/views/livewire/vet-login.blade.php
+++ resources/views/livewire/vet-login.blade.php
@@ -10,27 +10,15 @@
     @if ($error)
         <x-badge negative label="{{$error}}" />
     @endif
-    @if ($adm)
-        <x-input wire:model.lazy="adm_user" label="Username"
-        class="{{$error!=''?'ring-secondary-red':''}}"/>
-    @else
-    <x-input wire:model.defer="user" label="รหัสร้านค้า"
+    
+    <x-input wire:model.defer="user" label="Username"
     class="{{$error!=''?'ring-secondary-red':''}}" />
-    <!-- x-select
-        label="ชื่อคลินิก หรือ โรงพยาบาลสัตว์"
-        placeholder="ชื่อคลินิก หรือ โรงพยาบาลสัตว์"
-        :async-data="route('vets')"
-        option-label="name"
-        option-value="id"
-        wire:model.lazy="user" 
-         :options="$vet_list" -->
-            
-    @endif
+
     <x-inputs.password class="py-4 {{$error!=''?'ring-secondary-red':''}}" label="รหัสผ่าน" placeholder="รหัสผ่าน" wire:model.defer="password" />
 
-    <div class="py-4">
+    {{-- <div class="py-4">
         <x-checkbox label="จดจำรหัสผ่านของฉันเอาไว้" wire:model.defer="remember_me" />
-    </div>
+    </div> --}}
     <div wire:loading.remove>
         <x-button class="my-4" lg right-icon="chevron-right" primary
                     wire:click="login" type="button" label="login" />

6fb59360a50b6a0fe7fbf6519b9f09046b36f49e
Ruttaphong <maggotgluon@gmail.com>
2023-10-15 13:01:01 +0700

    add logout function

--- app/Http/Livewire/Admin/RegisterDash.php
+++ app/Http/Livewire/Admin/RegisterDash.php
@@ -4,9 +4,15 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Client;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
 
 class RegisterDash extends Component
 {
+    public function logout(){
+        Auth::guard('web')->logout();
+        return redirect('/');
+    }
     public function render()
     {
         $client = Client::all();

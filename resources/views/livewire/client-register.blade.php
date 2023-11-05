7728c48376d752afefde46753369c158127f88f2
Ruttaphong <maggotgluon@gmail.com>
2023-11-05 12:15:46 +0700

    update visual

--- resources/views/livewire/client-register.blade.php
+++ resources/views/livewire/client-register.blade.php
@@ -14,8 +14,8 @@
     <div class="text-center absolute inset-0 z-50 {{$currentStep>1? '' : 'hidden'}}" wire:loading>
         <img class="m-auto" src="{{url('/loading.gif')}}"/>
     </div>
-    <div class="flex justify-around relative {{$currentStep>=2? '' : 'hidden'}}">
-        <progress value={{$currentStep}} max="5" style="
+    <div class="flex justify-around relative {{$currentStep>=4? '' : 'hidden'}}">
+        <progress value={{$currentStep-2}} max="5" style="
             position: absolute;
             top: calc(50% - .25rem);
             bottom: calc(50% - .25rem);
